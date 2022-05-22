1759aa5bb83604eae042d308e8a6774797f63d10
Camilo Rodriguez <camilo@sdkconsultoria.com>
2022-05-22 12:05:00 -0500

    Installed phpunit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Sdkconsultoria\Core\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_the_application_returns_a_successful_response()
+    {
+    }
+}
