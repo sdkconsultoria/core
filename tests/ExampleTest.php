b355d74b31bff453bbd6d73099615bab8bcba191
Camilo Rodriguez <camilo@sdkconsultoria.com>
2022-05-29 12:52:01 -0500

    Configured phpunit

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,12 +6,19 @@ use Orchestra\Testbench\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('database.default', 'testbench');
+
+        $app['config']->set('database.connections.testbench', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+    }
+
     public function test_the_application_returns_a_successful_response()
     {
+        $this->loadLaravelMigrations(['--database' => 'testbench']);
     }
 }
