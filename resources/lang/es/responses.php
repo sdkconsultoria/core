c05d560dd99fbba92356d8d7a8a2ec2fd983af32
Camilo Rodríguez <camilo@sdkconsultoria.com>
2023-04-12 20:18:34 -0600

    Added translation

--- resources/lang/es/responses.php
+++ resources/lang/es/responses.php
@@ -4,6 +4,7 @@ return [
     '200' => 'Solicitud correcta.',
     '400' => 'Solicitud incorrecta.',
     '401' => 'Solicitud No autorizada.',
+    '403' => 'Solicitud No autorizada.',
     '404' => 'El recurso solicitado no existe.',
     '500' => 'Error del servidor.',
 ];
