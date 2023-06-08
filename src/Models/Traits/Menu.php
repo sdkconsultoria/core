8395517a1e1acc3db03fd8a22335e48f29b4ef38
Camilo Rodríguez <camilo@sdkconsultoria.com>
2023-06-08 12:29:38 -0600

    Added default search

--- src/Models/Traits/Menu.php
+++ src/Models/Traits/Menu.php
@@ -6,18 +6,18 @@ use Base;
 
 trait Menu
 {
-    public static function makeMenu(string $icon, array $extra_urls = [])
+    public static function makeMenu(string $icon, array $extra_urls = [], $default_search = '')
     {
         $called_class = get_called_class();
         $model = new $called_class;
         $end_point = $model->getApiEndpoint();
-
         return [
             'name' => $model->getTranslations()['plural'],
             'icon' => Base::icon($icon, ['class' => 'h-6 w-6']),
             'url' =>  $end_point.'.index',
             'crud' => $end_point,
             'extra_urls' => $extra_urls,
+            'default_search' => $default_search,
         ];
     }
 }
